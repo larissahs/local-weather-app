5873dba18a81ec22b592ad23a9474973ab5b7a64
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-05-19 10:17:03 -0700

    commit changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '436307574b3e34845b3a43f56804cda8',
+  baseUrl: 'http://',
 };
 
 /*
